9f2dc52105276e07f2b58fdb434cd62b47331a19
Chintharla Akshaya <chintharlaakshaya@gmail.com>
2025-08-10 19:52:28 +0530

    Initial commit

---
+++ src/oops/Bike_Implementation.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class Bike_Implementation {
+	
+	public static void main(String[] args) {
+		Bike bike=new Bike("Honda",1990);
+		System.out.println(bike);
+		
+		/*(Bike newbike=new Bike("Splender_plus",2000);
+		System.out.println(newbike);*/
+		
+		Bike demobike=new Bike();
+		
+		
+	}
+
+}
